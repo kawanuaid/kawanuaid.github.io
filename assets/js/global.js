5f20b992a6eff0c81b4ddb85653dc89e4958649e
Ertomedia <support@ertomedia.email>
2020-01-11 00:37:11 +0800

    Initial commits

---
+++ assets/js/global.js
@@ -0,0 +1,11 @@
+    //
+    //   WebFontLoader
+    //
+    
+    WebFont.load({
+        custom: {
+        families: [
+            'Inter:n1,i1,n2,i2,n3,i3,n4,i4,n5,i5,n6,i6,n7,i7,n8,i8,n9,i9'
+        ]
+        }
+    });
